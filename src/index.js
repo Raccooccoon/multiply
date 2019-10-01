c1b6b200b4347e48c450127ef3be0a65b77721b2
Victory <raccooccon@gmail.com>
2019-10-01 16:02:44 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let strA = first.split("").reverse();
+  let strB = second.split("").reverse();
+
+  let memo = [];
+
+  for (let i = 0; i < strA.length; i++) {
+    for (let j = 0; j < strB.length; j++) {
+      let elem = strA[i] * strB[j];
+      memo[i + j] = memo[i + j] ? memo[i + j] + elem : elem;
+    }
+  }
+
+  for (let i = 0; i < memo.length; i++) {
+    let number = memo[i] % 10;
+    let jump = Math.floor(memo[i] / 10);
+    memo[i] = number;
+
+    if (memo[i + 1]) {
+      memo[i + 1] += jump;
+    }
+    else if (jump != 0) {
+      memo[i + 1] = jump;
+    }
+  }
+
+  return memo.reverse().join('');
 }
+
+
